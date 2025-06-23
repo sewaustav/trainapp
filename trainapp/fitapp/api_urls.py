6e15059da0286a214f8872d72617b2d0ca2da65c
sewaustav <sewabobrov04@gmail.com>
2025-06-23 23:25:19 +0200

    Update api_urls.py

--- trainapp/fitapp/api_urls.py
+++ trainapp/fitapp/api_urls.py
@@ -1,8 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import *
-from rest_framework.authtoken.views import obtain_auth_token
-
 
 router = DefaultRouter()
 router.register(r'exercise', FitapplistViewSet, basename='exercise')
@@ -12,9 +10,9 @@ router.register(r'dprogram', DprogramlistViewSet, basename='dprogram')
 router.register(r'program_exercise', ProgramExerciselistViewSet, basename='program_exercise')
 router.register(r'workout_result', WorkoutResultlistViewSet, basename='workout_result')
 router.register(r'workout_result_set', WorkoutResultSetlistViewSet, basename='workout_result_set')
+router.register(r'schedule', FutureWorkoutViewSet, basename='schedule')
+router.register(r'schedule/exercises', FutureWorkoutExerciseViewSet, basename='schedule_exercises')
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    # path('api/token/', obtain_auth_token, name='api_token_auth'),
-    path('api/programs/delete/<str:name>', delete_program_by_name, name='delete-pr'),
 ]
